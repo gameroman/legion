0c61fa7876484e9ed45003cd1a236ad87309e6b2
Jerome <jerome.renaux@gmail.com>
2023-07-15 21:25:40 +0200

    Player selection

--- client/src/Arena.ts
+++ client/src/Arena.ts
@@ -8,6 +8,12 @@ export class Arena extends Phaser.Scene
     localPlayer;
     gridCorners;
     gridMap: Map<string, Player> = new Map<string, Player>();
+    playersMap: Map<number, Player> = new Map<number, Player>();
+    selectedPlayer: Player | null = null;
+    highlight: Phaser.GameObjects.Graphics;
+    tileSize = 60;
+    gridWidth = 20;
+    gridHeight = 9;
 
     constructor() {
         super({ key: 'Arena' });
@@ -105,13 +111,16 @@ export class Arena extends Phaser.Scene
         }
     }
 
-    drawGrid() {
-        const tileSize = 60;
-        const gridWidth = 20;
-        const gridHeight = 9;
+    isSkip(x, y) {
+        const v = 3;
+        const skip = y < this.gridHeight/2 ? Math.max(0, v - y - 1) : Math.max(0, y - (this.gridHeight - v));
+        // Skip drawing the corners to create an oval shape
+        return (x < skip || x >= this.gridWidth - skip);
+    }
 
-        const totalWidth = tileSize * gridWidth;
-        const totalHeight = tileSize * gridHeight;
+    drawGrid() {
+        const totalWidth = this.tileSize * this.gridWidth;
+        const totalHeight = this.tileSize * this.gridHeight;
 
         // const gameWidth = Number(this.sys.game.config.width);
         // const gameHeight = Number(this.sys.game.config.height);
@@ -124,26 +133,19 @@ export class Arena extends Phaser.Scene
         // Create a graphics object
         let graphics = this.add.graphics().setDepth(2).setAlpha(0.5);
 
-        function isSkip(x, y) {
-            const v = 3;
-            const skip = y < gridHeight/2 ? Math.max(0, v - y - 1) : Math.max(0, y - (gridHeight - v));
-            // Skip drawing the corners to create an oval shape
-            return (x < skip || x >= gridWidth - skip);
-        }
-
         // Loop over each row
-        for (let y = 0; y < gridHeight; y++) {
+        for (let y = 0; y < this.gridHeight; y++) {
             // In each row, loop over each column
-            for (let x = 0; x < gridWidth; x++) {
+            for (let x = 0; x < this.gridWidth; x++) {
                 
-                if (isSkip(x, y)) continue;
+                if (this.isSkip(x, y)) continue;
                 // Set the fill style to transparent
                 graphics.fillStyle(0xffffff, 0);
-                graphics.fillRect(startX + x * tileSize, startY + y * tileSize, tileSize, tileSize);
+                graphics.fillRect(startX + x * this.tileSize, startY + y * this.tileSize, this.tileSize, this.tileSize);
                 
                 // Set the line style to white for the border
                 graphics.lineStyle(2, 0xffffff, 1);
-                graphics.strokeRect(startX + x * tileSize, startY + y * tileSize, tileSize, tileSize);
+                graphics.strokeRect(startX + x * this.tileSize, startY + y * this.tileSize, this.tileSize, this.tileSize);
             }
         }
 
@@ -161,19 +163,19 @@ export class Arena extends Phaser.Scene
              let pointerY = pointer.y - startY;
  
              // Calculate the grid coordinates of the pointer
-             let gridX = Math.floor(pointerX / tileSize);
-             let gridY = Math.floor(pointerY / tileSize);
+             let gridX = Math.floor(pointerX / this.tileSize);
+             let gridY = Math.floor(pointerY / this.tileSize);
  
              // Ensure the pointer is within the grid
-             if (gridX >= 0 && gridX < gridWidth && gridY >= 0 && gridY < gridHeight) {
+             if (gridX >= 0 && gridX < this.gridWidth && gridY >= 0 && gridY < this.gridHeight) {
                  // Clear the previous highlight
                  highlightGraphics.clear();
 
-                 if (isSkip(gridX, gridY)) return;
+                 if (this.isSkip(gridX, gridY)) return;
  
                  // Draw a new highlight over the hovered tile
                  highlightGraphics.fillStyle(0xffffff, 0.3); // Semi-transparent white
-                 highlightGraphics.fillRect(startX + gridX * tileSize, startY + gridY * tileSize, tileSize, tileSize);
+                 highlightGraphics.fillRect(startX + gridX * this.tileSize, startY + gridY * this.tileSize, this.tileSize, this.tileSize);
              } else {
                  // Clear the highlight if the pointer is outside the grid
                  highlightGraphics.clear();
@@ -186,14 +188,15 @@ export class Arena extends Phaser.Scene
             let pointerY = pointer.y - startY;
 
             // Calculate the grid coordinates of the pointer
-            let gridX = Math.floor(pointerX / tileSize);
-            let gridY = Math.floor(pointerY / tileSize);
+            let gridX = Math.floor(pointerX / this.tileSize);
+            let gridY = Math.floor(pointerY / this.tileSize);
 
-            if (isSkip(gridX, gridY)) return;
+            if (this.isSkip(gridX, gridY)) return;
 
             // Ensure the pointer is within the grid
-            if (gridX >= 0 && gridX < gridWidth && gridY >= 0 && gridY < gridHeight) {
+            if (gridX >= 0 && gridX < this.gridWidth && gridY >= 0 && gridY < this.gridHeight) {
                 console.log(`Clicked tile at grid coordinates (${gridX}, ${gridY})`);
+                this.handlePlayerSelect(this.gridMap[this.serializeCoords(gridX, gridY)]);
             }
         }, this);
 
@@ -203,6 +206,30 @@ export class Arena extends Phaser.Scene
         let scaleY = gameHeight / bg.height;
         let scale = Math.max(scaleX, scaleY);
         bg.setScale(scale).setAlpha(0.7);
+
+        this.input.keyboard.on('keydown', this.handleKeyDown, this);
+    }
+
+    handleKeyDown(event) {
+        // Check if the pressed key is a number
+        let number;
+        if (event.keyCode >= Phaser.Input.Keyboard.KeyCodes.ZERO && event.keyCode <= Phaser.Input.Keyboard.KeyCodes.NINE) {
+            // Convert the key code to a number
+            number = event.keyCode - Phaser.Input.Keyboard.KeyCodes.ZERO;
+        } else if (event.keyCode >= Phaser.Input.Keyboard.KeyCodes.NUMPAD_ZERO && event.keyCode <= Phaser.Input.Keyboard.KeyCodes.NUMPAD_NINE) {
+            // Convert the key code to a number (for numpad keys)
+            number = event.keyCode - Phaser.Input.Keyboard.KeyCodes.NUMPAD_ZERO;
+        } else {
+            return;
+        }
+        this.handlePlayerSelect(this.playersMap[number]);
+    }
+
+    handlePlayerSelect(player: Player) {
+        if (!player || !player.isPlayer) return;
+        if (this.selectedPlayer) this.selectedPlayer.toggleSelect();
+        this.selectedPlayer = player;
+        this.selectedPlayer.toggleSelect();
     }
 
     createAnims() {
@@ -223,14 +250,42 @@ export class Arena extends Phaser.Scene
             const x = character.x * 60 + this.gridCorners.startX + 30;
             const y = character.y * 60 + this.gridCorners.startY - 10;
 
-            const player = new Player(this, x, y, i + 1, character.frame, isPlayer);
+            const player = new Player(this, character.x, character.y, x, y, i + 1, character.frame, isPlayer);
             this.gridMap[this.serializeCoords(character.x, character.y)] = player;
+
+            if (isPlayer) {
+                this.playersMap[i + 1] = player;
+            }
         }, this);
     }
 
     serializeCoords(x, y) {
         return `${x},${y}`;
     }
+
+    highlightCells(gridX, gridY, radius) {
+        // Create a new Graphics object to highlight the cells
+        if (!this.highlight) this.highlight = this.add.graphics();
+        this.highlight.clear();
+        this.highlight.fillStyle(0xffd700, 0.7); // Use golden color
+    
+        // Iterate over each cell in the grid
+        for (let y = -radius; y <= radius; y++) {
+            for (let x = -radius; x <= radius; x++) {
+                if (this.isSkip(gridX + x, gridY + y)) continue;
+                // Check if the cell is within the circle
+                if (x * x + y * y <= radius * radius) {
+                    // Calculate the screen position of the cell
+                    let posX = this.gridCorners.startX + (gridX + x) * this.tileSize;
+                    let posY = this.gridCorners.startY + (gridY + y) * this.tileSize;
+    
+                    // Draw a rectangle around the cell
+                    this.highlight.fillRect(posX, posY, this.tileSize, this.tileSize);
+                }
+            }
+        }
+    }
+    
 
     create ()
     {
--- client/src/Player.ts
+++ client/src/Player.ts
@@ -1,9 +1,21 @@
 export class Player extends Phaser.GameObjects.Container {
-    sprite;
-    numKey;
+    sprite: Phaser.GameObjects.Sprite;
+    numKey: Phaser.GameObjects.Text;
+    selectionOval: Phaser.GameObjects.Graphics;
+    isPlayer: boolean = false;
+    arena: Phaser.Scene;
+    gridX: number;
+    gridY: number;
+    distance: number;
 
-    constructor(scene, x, y, num, texture, isPlayer) {
+    constructor(scene: Phaser.Scene, gridX: number, gridY: number, x: number, y: number, num: number, texture: string, isPlayer: boolean) {
         super(scene, x, y);
+        this.setDepth(3);
+        this.isPlayer = isPlayer;
+        this.gridX = gridX;
+        this.gridY = gridY;
+        this.arena = this.scene.scene.get('Arena');
+        this.distance = 2;
 
         // Create the sprite using the given key and add it to the container
         this.sprite = scene.add.sprite(0, 0, texture);
@@ -11,16 +23,32 @@ export class Player extends Phaser.GameObjects.Container {
 
         // Create a text object to display the player's name and score, and add it to the container
         if (isPlayer) {
-            this.numKey = scene.add.text(30, 70, num, { fontSize: '16px', color: '#fff', stroke: '#000', strokeThickness: 3 }).setOrigin(1,1);
+            this.numKey = scene.add.text(30, 70, num.toString(), { fontSize: '16px', color: '#fff', stroke: '#000', strokeThickness: 3 }).setOrigin(1,1);
             this.add(this.numKey);
+
+            // Create a Graphics object for the selection oval
+            this.selectionOval = scene.add.graphics();
+            this.selectionOval.lineStyle(4, 0xffd700, 1);
+            this.selectionOval.strokeEllipse(0, 55, 70, this.sprite.height / 4);
+            this.selectionOval.setVisible(false);
+            this.add(this.selectionOval);
+
+            this.moveTo(this.numKey, 2);
+            this.moveTo(this.sprite, 1);
+
         } else {
             this.sprite.flipX = true;
         }
 
         // Add the container to the scene
         scene.add.existing(this);
-        this.setDepth(3);
 
         this.sprite.play(`${texture}_anim`);
+    }
+
+    toggleSelect() {
+        this.selectionOval.setVisible(!this.selectionOval.visible);
+        // @ts-ignore
+        this.arena.highlightCells(this.gridX, this.gridY, this.distance);
     }
 }
