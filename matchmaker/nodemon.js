00f4c789ff1a5e8c6b197b33e42bf3076823073e
Jerome <jerome.renaux@gmail.com>
2024-07-30 16:50:27 +0200

    Fix HMR for server and matchmaker + status effect propagation bug

--- matchmaker/nodemon.js
+++ matchmaker/nodemon.js
@@ -1,12 +1,20 @@
+const nodemon = require('nodemon');
+
 console.log(`Starting nodemon, Docker mode is ${process.env.IS_DOCKER ? 'enabled' : 'disabled'}`);
 const tsConfigFile = process.env.IS_DOCKER ? 'tsconfig.docker.json' : 'tsconfig.json';
 
-const { spawn } = require('child_process');
+nodemon({
+  watch: ["src"],
+  ext: "ts",
+  exec: `TS_NODE_PROJECT=${tsConfigFile} npx ts-node -r tsconfig-paths/register ./src/matchmaker.ts`,
+  ignore: ["src/**/*.spec.ts"]
+});
 
-const command = `TS_NODE_PROJECT=${tsConfigFile} npx ts-node -r tsconfig-paths/register ./src/matchmaker.ts`;
-
-const child = spawn(command, { shell: true, stdio: 'inherit' });
-
-child.on('exit', function (code) {
-  process.exit(code);
+nodemon.on('start', function () {
+  console.log('App has started');
+}).on('quit', function () {
+  console.log('App has quit');
+  process.exit();
+}).on('restart', function (files) {
+  console.log('App restarted due to: ', files);
 });
