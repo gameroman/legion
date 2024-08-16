26858799ce47b476e367a129ab3831db0149acf5
Vivian Obiako <vivianobiako1@gmail.com>
2024-08-16 08:38:27 +0800

    add setting dialog

--- client/src/components/HUD/PlayerInfo.tsx
+++ client/src/components/HUD/PlayerInfo.tsx
@@ -1,5 +1,5 @@
 // PlayerInfo.tsx
-import { h, Component } from 'preact';
+import { h, Component, createRef } from 'preact';
 import { route } from 'preact-router';
 import Modal from 'react-modal';
 import { PlayerProfileData } from "@legion/shared/interfaces";
@@ -10,33 +10,127 @@ interface Props {
   player: PlayerProfileData;
   position: string;
   isSpectator: boolean;
-  eventEmitter: any; 
-  isPlayerTeam: boolean; 
+  eventEmitter: any;
+  isPlayerTeam: boolean;
 }
 interface State {
   modalOpen: boolean;
   modalOpen1: boolean;
-  modalPos: any;
+  modalOpen2: boolean;
+  modalPos: any; 
+  musicCurrentValue: number; 
+  musicMinValue: number; 
+  musicMaxValue: number; 
+  sfxCurrentValue: number; 
+  sfxMinValue: number; 
+  sfxMaxValue: number; 
 }
 
 class PlayerInfo extends Component<Props, State> {
   events: any;
+
+  musicControlLineRef = createRef(); 
+  musicControlThumbRef = createRef(); 
+  sfxControlLineRef = createRef(); 
+  sfxControlThumbRef = createRef(); 
 
   constructor(props: Props) {
     super(props);
     this.state = {
       modalOpen: false,
       modalOpen1: false,
-      modalPos: null
+      modalOpen2: false,
+      modalPos: null, 
+      musicCurrentValue: 50, 
+      musicMinValue: 0, 
+      musicMaxValue: 100, 
+      sfxCurrentValue: 50, 
+      sfxMinValue: 0, 
+      sfxMaxValue: 100, 
     }
-    this.events = this.props.eventEmitter;
-  }
+    this.events = this.props.eventEmitter; 
+  } 
 
-  handleOpenModal = (e: any, isExit: boolean) => {
-    if (isExit) {
-      this.setState({ modalOpen: false, modalOpen1: true });
-      return;
+  componentDidMount() {
+    this.updateMusicControlThumb(); 
+    this.updateSFXControlThumb(); 
+  } 
+
+  componentDidUpdate(prevProps, prevState) { 
+    if (prevState.musicCurrentValue !== this.state.musicCurrentValue) { 
+      this.updateMusicControlThumb(); 
+    } 
+    if (prevState.sfxCurrentValue !== this.state.sfxCurrentValue) { 
+      this.updateSFXControlThumb(); 
     }
+  } 
+
+  updateMusicControlThumb = () => { 
+    if (this.musicControlLineRef.current && this.musicControlThumbRef.current) {
+      const musicControlLineRect = this.musicControlLineRef.current.getBoundingClientRect();
+      const musicThumbPosition = ((this.state.musicCurrentValue - this.state.musicMinValue) / (this.state.musicMaxValue - this.state.musicMinValue)) * musicControlLineRect.width;
+      this.musicControlThumbRef.current.style.left = `${musicThumbPosition}px`;
+    }
+  } 
+
+  handleMusicThumbDrag = (event) => {
+    event.preventDefault();
+
+    const musicControlLineRect = this.musicControlLineRef.current.getBoundingClientRect();
+
+    const handleMusicMouseMove = (e) => {
+      const newPosition = e.clientX - musicControlLineRect.left;
+      const newValue = ((newPosition / musicControlLineRect.width) * (this.state.musicMaxValue - this.state.musicMinValue)) + this.state.musicMinValue;
+      this.setState({ musicCurrentValue: Math.max(this.state.musicMinValue, Math.min(this.state.musicMaxValue, newValue)) });
+    };
+
+    const handleMusicMouseUp = () => {
+      document.removeEventListener('mousemove', handleMusicMouseMove);
+      document.removeEventListener('mouseup', handleMusicMouseUp);
+    };
+
+    document.addEventListener('mousemove', handleMusicMouseMove);
+    document.addEventListener('mouseup', handleMusicMouseUp);
+  }; 
+
+  updateSFXControlThumb = () => { 
+    if (this.sfxControlLineRef.current && this.sfxControlThumbRef.current) {
+      const sfxControlLineRect = this.sfxControlLineRef.current.getBoundingClientRect();
+      const sfxThumbPosition = ((this.state.sfxCurrentValue - this.state.sfxMinValue) / (this.state.sfxMaxValue - this.state.sfxMinValue)) * sfxControlLineRect.width;
+      this.sfxControlThumbRef.current.style.left = `${sfxThumbPosition}px`;
+    }
+  } 
+
+  handleSFXThumbDrag = (event) => {
+    event.preventDefault();
+
+    const sfxControlLineRect = this.sfxControlLineRef.current.getBoundingClientRect();
+
+    const handleSFXMouseMove = (e) => {
+      const newPosition = e.clientX - sfxControlLineRect.left;
+      const newValue = ((newPosition / sfxControlLineRect.width) * (this.state.sfxMaxValue - this.state.sfxMinValue)) + this.state.sfxMinValue;
+      this.setState({ sfxCurrentValue: Math.max(this.state.sfxMinValue, Math.min(this.state.sfxMaxValue, newValue)) });
+    };
+
+    const handleSFXMouseUp = () => {
+      document.removeEventListener('mousemove', handleSFXMouseMove);
+      document.removeEventListener('mouseup', handleSFXMouseUp);
+    };
+
+    document.addEventListener('mousemove', handleSFXMouseMove);
+    document.addEventListener('mouseup', handleSFXMouseUp);
+  };
+
+  handleOpenModal = (e: any, modalType: string) => {
+    if (modalType !== "menu_modal") {
+      if (modalType === "exit_modal") {
+        this.setState({ modalOpen: false, modalOpen1: true, modalOpen2: false });
+        return;
+      } else {
+        this.setState({ modalOpen: false, modalOpen1: false, modalOpen2: true });
+        return;
+      }
+    } 
 
     const elementRect = e.currentTarget.getBoundingClientRect();
 
@@ -51,7 +145,8 @@ class PlayerInfo extends Component<Props, State> {
   handleCloseModal = () => {
     this.setState({
       modalOpen: false,
-      modalOpen1: false
+      modalOpen1: false,
+      modalOpen2: false,
     });
   }
 
@@ -96,9 +191,9 @@ class PlayerInfo extends Component<Props, State> {
         zIndex: 10,
         backgroundColor: 'rgba(0, 0, 0, 0.75)',
       }
-    }; 
+    };
 
-    console.log("playerInfoData => ", this.props.player); 
+    console.log("playerInfoData => ", this.props.player);
 
     return (
       <div className={`player_info_container relative ${position === 'right' && 'player_info_container_right'}`} onClick={() => { }}>
@@ -107,7 +202,7 @@ class PlayerInfo extends Component<Props, State> {
           <span className="player_info_lvalue">{this.props.player.playerLevel}</span>
         </div>}
         <div className="player_info_player_profile">
-          <img src={`/avatars/${this.props.player.playerAvatar? this.props.player.playerAvatar: 'default'}.png`} />
+          <img src={`/avatars/${this.props.player.playerAvatar ? this.props.player.playerAvatar : 'default'}.png`} />
         </div>
         <div className="player_info">
           <p className="player_info_name">{this.props.player.playerName}</p>
@@ -119,7 +214,7 @@ class PlayerInfo extends Component<Props, State> {
           <div className="player_info_team"></div>
         </div>
         {this.props.isPlayerTeam && <div className="spectator_container_right">
-          {isSpectator &&  <div className="spectator_div">
+          {isSpectator && <div className="spectator_div">
             <div className="spectator" onClick={() => { }}>
               <img src="/HUD/applause_icon.png" alt="" />
             </div>
@@ -127,13 +222,18 @@ class PlayerInfo extends Component<Props, State> {
               <img src="/HUD/donate_icon.png" alt="" />
             </div>
           </div>}
-          <div className="spectator" onClick={(e) => this.handleOpenModal(e, false)}>
+          <div className="spectator" onClick={(e) => this.handleOpenModal(e, "menu_modal")}>
             <img src="/HUD/settings_icon.png" alt="" />
           </div>
         </div>}
         <Modal isOpen={this.state.modalOpen} style={customStyles} onRequestClose={this.handleCloseModal}>
-          <div className="exit_game_label" onClick={(e) => this.handleOpenModal(e, true)}>
-            <p>Abandon Game!</p>
+          <div>
+            <div className="game_setting" onClick={(e) => this.handleOpenModal(e, "setting_modal")}>
+              <p>Setting</p>
+            </div>
+            <div className="exit_game_label" onClick={(e) => this.handleOpenModal(e, "exit_modal")}>
+              <p>Abandon Game!</p>
+            </div>
           </div>
         </Modal>
         <Modal isOpen={this.state.modalOpen1} onRequestClose={this.handleCloseModal} style={customStyles1}>
@@ -142,6 +242,43 @@ class PlayerInfo extends Component<Props, State> {
             <div className="flex gap_4">
               <div className="game_leave_btn" onClick={this.handleExit}>Leave</div>
               <div className="game_leave_btn" onClick={this.handleCloseModal}>Cancel</div>
+            </div>
+          </div>
+        </Modal>
+        <Modal isOpen={this.state.modalOpen2} onRequestClose={this.handleCloseModal} style={customStyles1}>
+          <div className="setting_menu flex flex_col gap_4">
+            <div className="setting_dialog">
+              <div className="setting_dialog_keyboard">
+                Keyboard layout:
+              </div>
+              <div className="setting_dialog_keyboard_btn_container flex justify_center gap_4">
+                <div className="setting_menu_btn">Azerty</div>
+                <div className="setting_menu_btn">Qwerty</div>
+              </div>
+              <div className="setting_dialog_control_bar_container">
+                <div className="setting_dialog_control_name">Music volume: </div> 
+                <div className="setting_dialog_contol_lable_start">{this.state.musicMinValue}</div>
+                <div className="setting_dialog_control_bar">
+                  <div className="setting_dialog_control_line" ref={this.musicControlLineRef}>
+                    <div className="setting_dialog_control_thumb" ref={this.musicControlThumbRef} onMouseDown={this.handleMusicThumbDrag}></div>
+                  </div>
+                </div>
+                <div className="setting_dialog_control_label_end">{this.state.musicMaxValue}</div>
+              </div>
+              <div className="setting_dialog_control_bar_container">
+                <div className="setting_dialog_control_name">SFX volume: </div> 
+                <div className="setting_dialog_contol_lable_start">{this.state.sfxMinValue}</div>
+                <div className="setting_dialog_control_bar">
+                  <div className="setting_dialog_control_line" ref={this.sfxControlLineRef}>
+                    <div className="setting_dialog_control_thumb" ref={this.sfxControlThumbRef} onMouseDown={this.handleSFXThumbDrag}></div>
+                  </div>
+                </div>
+                <div className="setting_dialog_contol_lable_end">{this.state.sfxMaxValue}</div>
+              </div>
+            </div>
+            <div className="justify_center flex gap_4">
+              {/* <div className="setting_exit_btn" onClick={() => {}}>Leave</div> */}
+              <div className="setting_menu_btn" onClick={this.handleCloseModal}>Exit</div>
             </div>
           </div>
         </Modal>
