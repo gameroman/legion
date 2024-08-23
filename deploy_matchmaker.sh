b3df69365136d5e0dfba22951eb3b13bf65fe5d9
Jerome <jerome.renaux@gmail.com>
2024-08-23 15:56:20 +0200

    Full matchmaker deployment script

--- deploy_matchmaker.sh
+++ deploy_matchmaker.sh
@@ -9,10 +9,10 @@ DEBUG_FLAGS="--no-cache --progress=plain"
 # docker build $DEBUG_FLAGS -f matchmaker/Dockerfile.prod -t $IMAGE --platform linux/amd64 .
 docker build -f matchmaker/Dockerfile.prod -t $IMAGE --platform linux/amd64 .
 
-# docker push $IMAGE
+docker push $IMAGE
 
-# gcloud run deploy $SERVICE_NAME \
-#   --image $IMAGE \
-#   --platform managed \
-#   --region $REGION \
-#   --allow-unauthenticated
+gcloud run deploy $SERVICE_NAME \
+  --image $IMAGE \
+  --platform managed \
+  --region $REGION \
+  --allow-unauthenticated
