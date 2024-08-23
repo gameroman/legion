74f4abcf0492910665fd66b2550b4e5123350c09
Jerome <jerome.renaux@gmail.com>
2024-08-23 11:20:20 +0200

    Update matchmaker deployment script

--- deploy_matchmaker.sh
+++ deploy_matchmaker.sh
@@ -2,9 +2,10 @@
 
 SERVICE_NAME="matchmaker"
 IMAGE="gcr.io/legion-32c6d/$SERVICE_NAME"
+TAG="latest"
 REGION="us-central1"
 
-docker tag legion-$SERVICE_NAME $IMAGE
+docker build -f matchmaker/Dockerfile -t $IMAGE --platform linux/amd64 .
 docker push $IMAGE
 
 gcloud run deploy $SERVICE_NAME \
