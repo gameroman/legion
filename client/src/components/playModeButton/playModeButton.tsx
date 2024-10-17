787e8b9104cb1a28396f6e44c4fc0e7e35b44acd
Jerome <jerome.renaux@gmail.com>
2024-10-17 10:18:15 +0200

    Spelling mistake

--- client/src/components/playModeButton/playModeButton.tsx
+++ client/src/components/playModeButton/playModeButton.tsx
@@ -75,7 +75,7 @@ class PlayModeButton extends Component<ButtonProps> {
                 <div className="labelContainer">
                     <span className="label">{this.props.label}</span>
                     {this.props.isLobbies 
-                        ? (this.state.lobbiesCount > 0 && <span className="player"><span style={playerSpanStyle}>{this.state.lobbiesCount}</span> Opponents Waiting</span>)
+                        ? (this.state.lobbiesCount > 0 && <span className="player"><span style={playerSpanStyle}>{this.state.lobbiesCount}</span> {this.state.lobbiesCount === 1 ? 'Opponent' : 'Opponents'} Waiting</span>)
                         : (this.props.players && <span className="player"><span style={playerSpanStyle}>{this.props.players}</span> Players Queuing</span>)
                     }
                 </div>
