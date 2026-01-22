bd81c9b252a0ddd24e61c834bbb28316bffe4010
Roman <dev@rman.dev>
2026-01-22 21:42:26 +0000

    update more files

--- client/types/declarations.d.ts
+++ client/types/declarations.d.ts
@@ -1,5 +1,8 @@
 // Extend the NodeRequire type with the context method
 declare interface NodeRequire {
-    context: (directory: string, useSubdirectories: boolean, regExp: RegExp) => __WebpackModuleApi.RequireContext;
-  }
-  
+  context: (
+    directory: string,
+    useSubdirectories: boolean,
+    regExp: RegExp,
+  ) => __WebpackModuleApi.RequireContext;
+}
